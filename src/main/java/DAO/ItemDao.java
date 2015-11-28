d3f6e4783bf7d977fc389a4435db008ade818701
Luiz Carlos Szpikula Junior <luiz.szpikula@alfaconcursos.com.br>
2015-11-28 20:15:43 -0200

    dao pedidos e itens

--- src/main/java/DAO/ItemDao.java
+++ src/main/java/DAO/ItemDao.java
@@ -1,18 +1,25 @@
 package DAO;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import MODEL.Item;
 
 public interface ItemDao {
 	
-	public void inserir(Item i);
+	public void inserir(Item item);
 
-	public void atualizar(Item i);
+	public void atualizar(Item item);
 
-	public void excluir(Item i);
+	public void excluir(Item item);
 
 	public Item buscar(int id);
+	
+	public int pedido_total_itens(int pedito_id);
+	
+	public BigDecimal pedido_valor_total(int pedido_id);
+	
+	public BigDecimal pedido_valor_desconto(int pedido_id);
 
-	public List<Item> listar();
+	public List<Item> listar(int pedido_id);
 }
--- src/main/java/DAO/PedidoDao.java
+++ src/main/java/DAO/PedidoDao.java
@@ -6,13 +6,15 @@ import MODEL.Pedido;
 
 public interface PedidoDao extends templateDao{
 	
-	public void inserir(Pedido p);
+	public void inserir(Pedido pedido);
 
-	public void atualizar(Pedido p);
+	public void atualizar(Pedido pedido);
 
-	public void excluir(Pedido p);
+	public void excluir(Pedido pedido);
 
 	public Pedido buscar(int id);
 
+	public List<Pedido> listar_pedidos_cliente(int cliente_id);
+	
 	public List<Pedido> listar();
 }
